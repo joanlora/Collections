a9a58be1da6796a4161a2f3b3647a32eead9ffb4
Joan Lora <Joan.Lora@outlook.com>
2023-01-26 03:22:53 -0500

    added a mutable map to UnmodifiableVsImmutable.java and learned about .copyof / .put

--- src/main/java/com/monotonic/collections/_6_operations/before/UnmodifiableVsImmutable.java
+++ src/main/java/com/monotonic/collections/_6_operations/before/UnmodifiableVsImmutable.java
@@ -9,5 +9,37 @@ public class UnmodifiableVsImmutable
         var mutableCountryToPopulation = new HashMap<>();
         mutableCountryToPopulation.put("UK", 67);
         mutableCountryToPopulation.put("USA", 328);
+        //mutableCountryToPopulation.put("Wessex", null); .copyof does not allow null values
+        mutableCountryToPopulation.put("DR", 10);
+
+        var unmodifiable = Collections.unmodifiableMap(mutableCountryToPopulation);
+        var copied = Map.copyOf(mutableCountryToPopulation);
+        var mutableCopy = new HashMap<>(mutableCountryToPopulation);
+
+        /*var countryToPop = Map.of("UK",67,"USA","328");
+        System.out.println(countryToPop);*/
+
+        // countryToPop.put("Germany", 83); throws an error
+
+        System.out.println("mutableCountryToPopulation = " + mutableCountryToPopulation);
+        System.out.println("unmodifiable = " + unmodifiable);
+        System.out.println("copied = " + copied);
+
+        /*mutableCopy.put("Germany",83);
+        System.out.println("mutable copied: " + mutableCopy);
+
+        System.out.println("PUT");
+        mutableCountryToPopulation.put("Germany", 83);
+
+        System.out.println("mutableCountryToPopulation = " + mutableCountryToPopulation);
+        System.out.println("unmodifiable = " + unmodifiable);
+        System.out.println("copied = " + copied);*/
+
+        // ctrl shift slash to make block comment (intellij)
+        // ctrl slash to make line comment (intellij)
+        // - JL 1/26/23
+
+
+
     }
 }
